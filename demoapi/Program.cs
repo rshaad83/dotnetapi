c073796614021a5fa13315221ad74fdd791e2c51
Arshad Azeem <arshadazeem@gmail.com>
2018-05-10 13:24:52 -0400

    addedn appInsights support

--- demoapi/Program.cs
+++ demoapi/Program.cs
@@ -19,6 +19,7 @@ namespace demoapi
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>()
                 .Build();
     }
